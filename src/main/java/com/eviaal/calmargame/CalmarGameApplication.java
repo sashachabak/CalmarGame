ed656883e3a2502b6328239c972e98d3e6ec091c
EviaaL <eviaal@icloud.com>
2021-11-03 21:21:26 +0200

    Initial commit

---
+++ src/main/java/com/eviaal/calmargame/CalmarGameApplication.java
@@ -0,0 +1,13 @@
+package com.eviaal.calmargame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalmarGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalmarGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eviaal/calmargame/CalmarGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviaal.calmargame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalmarGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
